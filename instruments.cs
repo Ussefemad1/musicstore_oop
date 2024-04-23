4f1c7eee53e8d5d41bfec0348853b1914b726b85
Norhanoraby <164951074+Norhanoraby@users.noreply.github.com>
2024-04-23 16:12:30 +0200

    Add files via upload

---
+++ instruments.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+
+
+namespace projectoop
+{
+    public partial class instruments : Form
+    {
+        public instruments()
+        {
+            InitializeComponent();
+        }
+        SqlConnection connection = new SqlConnection(@"Data Source=localhost;Initial Catalog=Instruments;Integrated Security=True;Encrypt=True;Trust Server Certificate=True");
+        private void guna2GradientPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
